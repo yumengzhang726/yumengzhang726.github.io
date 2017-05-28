4bc1a205714bc2751978b7995e485917e64f3969
yumeng zhang <yzha582@student.monash.edu>
2017-05-28 17:00:54 +1000

    1116

---
+++ assets/js/main5.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+
+  $('.Sophie').mouseenter(function() {
+     $('.but1').addClass('showme');
+   });
+
+   $('.Sophie').mouseleave(function() {
+     $('.but1').removeClass('showme');
+   });
+   $('.but').mouseenter(function() {
+      $('.but1').addClass('showme');
+    });
+
+    $('.but').mouseleave(function() {
+      $('.but1').removeClass('showme');
+    });
+
+    $('.but2').mouseenter(function() {
+       $('.but4').addClass('showme');
+     });
+
+     $('.but2').mouseleave(function() {
+       $('.but4').removeClass('showme');
+     });
+     $('.la').mouseenter(function() {
+        $('.but4').addClass('showme');
+      });
+
+      $('.la').mouseleave(function() {
+        $('.but4').removeClass('showme');
+      });
+
+      $('.but3').mouseenter(function() {
+         $('.but5').addClass('showme');
+       });
+
+       $('.but3').mouseleave(function() {
+         $('.but5').removeClass('showme');
+       });
+       $('.gor').mouseenter(function() {
+          $('.but5').addClass('showme');
+        });
+
+        $('.gor').mouseleave(function() {
+          $('.but5').removeClass('showme');
+        });
+
+
+
+
+
+
+
+
+
+
+  });
